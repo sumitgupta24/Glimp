7d45ae14f1415f805b1894d98596decf48527e49
Sumit Gupta <cricsumit24@gmail.com>
2025-12-18 21:51:19 +0530

    chore: setup express server and test route

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,17 @@ import express from 'express';
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+const route = express.Router();
+
+app.use(route)
+
+route.get('/api/v1',(req,res) => {
+    res.send('HI')
+})
 
 app.listen(port,() => {
     console.log(`Server listening on port ${port}`)

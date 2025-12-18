92496cf36ade16bb3d73761e5c1d0cf931f91f19
Sumit Gupta <cricsumit24@gmail.com>
2025-12-18 21:36:01 +0530

    Initialize backend with Express and dotenv configuration

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port,() => {
+    console.log(`Server listening on port ${port}`)
+})
